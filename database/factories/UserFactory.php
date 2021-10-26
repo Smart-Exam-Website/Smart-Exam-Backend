6a923bf1d20a696efcc2d8b892977c66c71b5577
haidyelnahass <haidyelnahass@hotmail.com>
2021-10-26 18:16:19 +0200

    created user migration, controller,model,factory, seeder

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'firstName' => $this->faker->name(),
+            'lastName' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'gender' => $this->faker->randomElement(['male', 'female']),
+            'image' => $this->faker->imageUrl($width = 300, $height = 300),
+            'phone' => $this->faker->phoneNumber(),
+            'type' => $this->faker->randomElement(['admin', 'student', 'instructor']),
+            'password' => Hash::make('secret'), // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
