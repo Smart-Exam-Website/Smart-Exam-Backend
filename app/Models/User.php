b20a4b9278bd97e0b06f8554cc68c9f02744017a
haidyelnahass <haidyelnahass@hotmail.com>
2021-10-29 16:01:07 +0200

    created admin migration,model,seeder,factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,5 +60,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Student::class);
     }
+    public function admin()
+    {
+        return $this->hasOne(Admin::class);
+    }
 
 }
