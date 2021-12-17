921b00d2e886cfa9701d33d29f02fe1899994cdd
haidyelnahass <haidyelnahass@hotmail.com>
2021-12-17 19:56:26 +0200

    minor fix

--- database/migrations/2021_12_02_142521_create_answers_table.php
+++ database/migrations/2021_12_02_142521_create_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTakesTable extends Migration
+class CreateAnswersTable extends Migration
 {
     /**
      * Run the migrations.
