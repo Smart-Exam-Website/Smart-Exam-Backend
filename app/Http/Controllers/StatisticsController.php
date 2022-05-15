18ff5c1a6d2f9700175fa70dd34ed97b59ad71fe
nouraan-ahmed <norahahmed1999@gmail.com>
2022-05-15 18:39:04 +0200

    exam histogram bar chart

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -5,51 +5,64 @@ namespace App\Http\Controllers;
 use App\Models\Answer;
 use App\Models\Exam;
 use App\Models\User;
+use App\Models\ExamStudent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use ConsoleTVs\Charts\Facades\Charts;
+
 class StatisticsController extends Controller
 {
     public function index(Exam $exam)
     {
-		$error = "";
-		if(!$exam) {
-			$error = "No exam found with this id!";
-		}
-		$questions = $exam->questions;
+        $error = "";
+        if (!$exam) {
+            $error = "No exam found with this id!";
+        }
+        $questions = $exam->questions;
 
-		$questionsData = [];
-
-
-		foreach ($questions as $question) {
-			$answers = Answer::where(['exam_id' => $exam->id, 'question_id' => $question->id])->get();
-			$questionData = ['good' => 0, 'bad' => 0, 'very bad' => 0];
-			foreach($answers as $answer) {
-				if($answer->isMarked == true) {
-					if($answer->questionMark == 0) {
-						$questionData['very bad']++; 
-					} else if ($answer->questionMark < $question->pivot->mark) {
-						$questionData['good']++;
-					}
-				}
-				
-			}
-			array_push($questionsData, $questionData);
-		}
+        $questionsData = [];
 
 
+        foreach ($questions as $question) {
+            $answers = Answer::where(['exam_id' => $exam->id, 'question_id' => $question->id])->get();
+            $questionData = ['good' => 0, 'bad' => 0, 'very bad' => 0];
+            foreach ($answers as $answer) {
+                if ($answer->isMarked == true) {
+                    if ($answer->questionMark == 0) {
+                        $questionData['very bad']++;
+                    } else if ($answer->questionMark < $question->pivot->mark) {
+                        $questionData['good']++;
+                    }
+                }
+            }
+            array_push($questionsData, $questionData);
+        }
 
-    	// $students = User::where('type','student')->get();
-    	// $instructors = User::where('type','instructor')->get();
-    	// $admins = User::where('type','admin')->get();
-    	// $student_count = count($students);    	
-    	// $instructor_count = count($instructors);
-    	// $admin_count = count($admins); 
+
+        //bar Chart
+
+        $marks = [];
+
+        $ExamStudents = ExamStudent::where(['exam_id' => $exam->id])->get();
+        $examMark = $exam->totalMark;
+        $mark100 = ExamStudent::where('totalMark', '>', 80)->where('totalMark', '<=', 100)->count();
+        $mark80 = ExamStudent::where('totalMark', '>', 60)->where('totalMark', '<=', 80)->count();
+        $mark60 = ExamStudent::where('totalMark', '>', 40)->where('totalMark', '<=', 60)->count();
+        $mark40 = ExamStudent::where('totalMark', '>', 20)->where('totalMark', '<=', 40)->count();
+        $mark20 = ExamStudent::where('totalMark', '>', 0)->where('totalMark', '<=', 20)->count();
+        $mark0 = ExamStudent::where('totalMark', 0)->count();
+
+        // $students = User::where('type','student')->get();
+        // $instructors = User::where('type','instructor')->get();
+        // $admins = User::where('type','admin')->get();
+        // $student_count = count($students);
+        // $instructor_count = count($instructors);
+        // $admin_count = count($admins);
         // $data =[];
         // $data['students'] = $student_count;
         // $data['instructors'] = $instructor_count;
-        // $data['admins'] = $admin_count; 
-        
-        return view('charts',compact('questionsData', 'error'));
+        // $data['admins'] = $admin_count;
+
+        return view('charts', compact('mark0', 'mark20', 'mark40', 'mark60', 'mark80', 'mark100', 'questionsData', 'error'));
     }
 }
--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -2,12 +2,13 @@
 
 <head>
     <title>Exam Statistics</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 
 <body>
-    <h2 style="margin:50px 0px 0px 0px;text-align: center;">Exam Statistics</h2>
-
+    <h2 class="title">Exam Statistics</h2>
+    <div id="barchart"></div>
     <?php for ($i = 0; $i < count($questionsData); $i++) {
         $chartId = "piechart " . $i;
         $questionData = $questionsData[$i];
@@ -17,49 +18,82 @@
 
 
     ?>
-                <table class="columns">
-                    <tr>
-                    <?php } else { ?>
-                    </tr>
-                </table>
-                <table class="columns">
-                    <tr>
-                <?php }
+    <table class="columns">
+        <tr>
+            <?php } else { ?>
+        </tr>
+    </table>
+    <table class="columns">
+        <tr>
+            <?php }
             } ?>
 
-                <td>
-                    <div id="<?php echo $chartId ?>" style="width: 150px; height: 150px; margin-left: 235px">
-                    </div>
-                </td>
-                <script type="text/javascript">
-                    var good_count = <?php echo $questionData['good']; ?>;
-                    var bad_count = <?php echo $questionData['bad']; ?>;
-                    var very_bad_count = <?php echo $questionData['very bad']; ?>;
-                    google.charts.load('current', {
-                        'packages': ['corechart']
-                    });
-                    google.charts.setOnLoadCallback(drawChart);
+            <td>
+                <div id="<?php echo $chartId; ?>" style="width: 150px; height: 150px; margin-left: 235px">
+                </div>
+            </td>
+            <script type="text/javascript">
+                var good_count = <?php echo $questionData['good']; ?>;
+                var bad_count = <?php echo $questionData['bad']; ?>;
+                var very_bad_count = <?php echo $questionData['very bad']; ?>;
+                google.charts.load('current', {
+                    'packages': ['corechart']
+                });
+                google.charts.setOnLoadCallback(drawChart);
 
 
-                    function drawChart() {
-                        var data = google.visualization.arrayToDataTable([
-                            ['User Type', 'Count'],
-                            ['Good', good_count],
-                            ['Bad', bad_count],
-                            ['Very bad', very_bad_count]
-                        ]);
-                        var options = {
-                            curveType: 'function',
-                            legend: {
-                                position: 'bottom'
-                            }
-                        };
-                        var chart = new google.visualization.PieChart(document.getElementById("<?php echo $chartId ?>"));
-                        chart.draw(data, options);
-                    }
-                </script>
+                function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                        ['User Type', 'Count'],
+                        ['Good', good_count],
+                        ['Bad', bad_count],
+                        ['Very bad', very_bad_count]
+                    ]);
+                    var options = {
+                        curveType: 'function',
+                        legend: {
+                            position: 'bottom'
+                        }
+                    };
+                    var chart = new google.visualization.PieChart(document.getElementById("<?php echo $chartId; ?>"));
+                    chart.draw(data, options);
+                }
+            </script>
+            <script type="text/javascript">
+                //bar chart
+
+                google.charts.load('current', {
+                    'packages': ['bar']
+                });
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+
+                    var data = google.visualization.arrayToDataTable([
+                        ['Mark Percent', 'Student Count'],
+                        ['0%', <?php echo $mark0; ?>],
+                        ['20%', <?php echo $mark20; ?>],
+                        ['40%', <?php echo $mark40; ?>],
+                        ['60%', <?php echo $mark60; ?>],
+                        ['80%', <?php echo $mark80; ?>],
+                        ['100%', <?php echo $mark100; ?>]
+
+                    ]);
+
+                    var options = {
+                        chart: {
+                            title: 'Bar Graph | Histogram for Exam Marks',
+                            subtitle: '@php echo date('Y-m-d H:i:s') @endphp',
+                        },
+                        bars: 'vertical'
+                    };
+                    var chart = new google.charts.Bar(document.getElementById('barchart'));
+                    chart.draw(data, google.charts.Bar.convertOptions(options));
+                }
+            </script>
             <?php } ?>
-
+        </tr>
+    </table>
 
 
 </body>
