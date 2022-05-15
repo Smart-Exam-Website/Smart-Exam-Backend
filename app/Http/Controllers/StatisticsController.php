eca9b22dd3f6d39e68eaef3ea8c9aa8e1a98c1ba
haidyelnahass <haidyelnahass@hotmail.com>
2022-05-15 20:02:19 +0200

    piechart adjustment

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -8,12 +8,13 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use ConsoleTVs\Charts\Facades\Charts;
+
 class StatisticsController extends Controller
 {
-    public function index(Exam $exam)
-    {
+	public function index(Exam $exam)
+	{
 		$error = "";
-		if(!$exam) {
+		if (!$exam) {
 			$error = "No exam found with this id!";
 		}
 		$questions = $exam->questions;
@@ -22,34 +23,56 @@ class StatisticsController extends Controller
 
 
 		foreach ($questions as $question) {
-			$answers = Answer::where(['exam_id' => $exam->id, 'question_id' => $question->id])->get();
-			$questionData = ['good' => 0, 'bad' => 0, 'very bad' => 0];
-			foreach($answers as $answer) {
-				if($answer->isMarked == true) {
-					if($answer->questionMark == 0) {
-						$questionData['very bad']++; 
-					} else if ($answer->questionMark < $question->pivot->mark) {
-						$questionData['good']++;
+			$questionData = ['Good' => 0, 'Fair' => 0, 'Bad' => 0];
+			if ($question->type != 'group') {
+				$answers = Answer::where(['exam_id' => $exam->id, 'question_id' => $question->id])->get();
+				foreach ($answers as $answer) {
+					if ($answer->isMarked == true) {
+						if ($answer->questionMark == 0) {
+							$questionData['Bad']++;
+						} else if ($answer->questionMark < $question->pivot->mark) {
+							$questionData['Fair']++;
+						} else {
+							$questionData['Good']++;
+						}
 					}
 				}
-				
+				array_push($questionsData, $questionData);
+			} else {
+				$groupQs = $question->questions;
+				foreach ($groupQs as $groupQ) {
+					$questionData = ['Good' => 0, 'Fair' => 0, 'Bad' => 0];
+					$answers = Answer::where(['exam_id' => $exam->id, 'question_id' => $groupQ->id])->get();
+					foreach ($answers as $answer) {
+						if ($answer->isMarked == true) {
+							if ($answer->questionMark == 0) {
+								$questionData['Bad']++;
+							} else if ($answer->questionMark < $question->pivot->mark) {
+								$questionData['Fair']++;
+							} else {
+								$questionData['Good']++;
+							}
+						}
+					}
+					array_push($questionsData, $questionData);
+				}
 			}
-			array_push($questionsData, $questionData);
+			
 		}
 
 
 
-    	// $students = User::where('type','student')->get();
-    	// $instructors = User::where('type','instructor')->get();
-    	// $admins = User::where('type','admin')->get();
-    	// $student_count = count($students);    	
-    	// $instructor_count = count($instructors);
-    	// $admin_count = count($admins); 
-        // $data =[];
-        // $data['students'] = $student_count;
-        // $data['instructors'] = $instructor_count;
-        // $data['admins'] = $admin_count; 
-        
-        return view('charts',compact('questionsData', 'error'));
-    }
+		// $students = User::where('type','student')->get();
+		// $instructors = User::where('type','instructor')->get();
+		// $admins = User::where('type','admin')->get();
+		// $student_count = count($students);    	
+		// $instructor_count = count($instructors);
+		// $admin_count = count($admins); 
+		// $data =[];
+		// $data['students'] = $student_count;
+		// $data['instructors'] = $instructor_count;
+		// $data['admins'] = $admin_count; 
+
+		return view('charts', compact('questionsData', 'error'));
+	}
 }
--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -8,33 +8,32 @@
 <body>
     <h2 style="margin:50px 0px 0px 0px;text-align: center;">Exam Statistics</h2>
 
-    <?php for ($i = 0; $i < count($questionsData); $i++) {
+    @for ($i = 0; $i < count($questionsData); $i++) 
+        @php
         $chartId = "piechart " . $i;
         $questionData = $questionsData[$i];
+        @endphp
 
-        if ($i % 2 == 0) {
-            if ($i == 0) {
-
-
-    ?>
+        @if ($i % 2 == 0) 
+            @if ($i == 0) 
                 <table class="columns">
                     <tr>
-                    <?php } else { ?>
+            @else
                     </tr>
                 </table>
                 <table class="columns">
                     <tr>
-                <?php }
-            } ?>
+            @endif
+        @endif
 
                 <td>
                     <div id="<?php echo $chartId ?>" style="width: 150px; height: 150px; margin-left: 235px">
                     </div>
                 </td>
                 <script type="text/javascript">
-                    var good_count = <?php echo $questionData['good']; ?>;
-                    var bad_count = <?php echo $questionData['bad']; ?>;
-                    var very_bad_count = <?php echo $questionData['very bad']; ?>;
+                    var good_count = <?php echo $questionData['Good']; ?>;
+                    var bad_count = <?php echo $questionData['Bad']; ?>;
+                    var fair_count = <?php echo $questionData['Fair']; ?>;
                     google.charts.load('current', {
                         'packages': ['corechart']
                     });
@@ -43,11 +42,12 @@
 
                     function drawChart() {
                         var data = google.visualization.arrayToDataTable([
-                            ['User Type', 'Count'],
+                            ['Type', 'Count'],
                             ['Good', good_count],
                             ['Bad', bad_count],
-                            ['Very bad', very_bad_count]
+                            ['Fair', fair_count]
                         ]);
+                        console.log(good_count, bad_count, fair_count);
                         var options = {
                             curveType: 'function',
                             legend: {
@@ -58,7 +58,7 @@
                         chart.draw(data, options);
                     }
                 </script>
-            <?php } ?>
+            @endfor
 
 
 
